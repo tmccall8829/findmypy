07ad7e5ebc4577d376810d898486209ab4dc2723
Tom McCall <tom.mccall@huntclub.com>
2023-08-24 16:10:13 -0700

    feat: static frontend

--- src/app.py
+++ src/app.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 
 from models.package import PackageURL
 from utils.api import FindMyPy
 
 app = FastAPI()
-
-
-@app.get("/")
-def index():
-    return (
-        "Append a PyPI package name to this base URL to get their GitHub Project URL."
-    )
+app.mount("/", StaticFiles(directory="ui", html=True), name="ui")
 
 
 @app.get("/{package_name}", response_model=PackageURL)
